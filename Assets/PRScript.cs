6a9b648d8fa9bf0af26269002d2ebe2b49a482eb
question0914 <895820518@qq.com>
2018-04-18 13:20:51 -0700

    Revert "update"
    This reverts commit e657d238555138c4856594ff675193c763dd9883.

--- Assets/PRScript.cs
+++ Assets/PRScript.cs
@@ -9,16 +9,7 @@ public class PRScript : MonoBehaviour {
 	public static bool IsPaused = false;
 	public GameObject PauseMenuUI;
 	public GameObject ResumeMenuUI;
-<<<<<<< HEAD
-	public GameObject FPPMenuUI;
-	public GameObject TPPMenuUI;
-	public GameObject FPPCamera;
-	public GameObject TPPCamera;
-	public GameObject PlayerAvatar;
-	public GameObject PlayerMenuUI;
-=======
 	public GameObject SGMenuUI;
->>>>>>> 207ad6f5145d6df9c59e8f4ab687efc6e272c0be
 
 	// Use this for initialization
 	void Start () {
@@ -55,21 +46,13 @@ public class PRScript : MonoBehaviour {
 		IsPaused = true;
 	}
 
-<<<<<<< HEAD
-	/*public void StartGame () {
-=======
 	public void StartGame () {
->>>>>>> 207ad6f5145d6df9c59e8f4ab687efc6e272c0be
 		Time.timeScale = 1f;
 		AudioListener.volume = 1F;
 		PauseMenuUI.SetActive (true);
 		SGMenuUI.SetActive (false);
 		IsPaused = false;
-<<<<<<< HEAD
-	}*/
-=======
 	}
->>>>>>> 207ad6f5145d6df9c59e8f4ab687efc6e272c0be
 
 	public void LoadMenu () {
 		Debug.Log ("loading menu...");
@@ -78,45 +61,4 @@ public class PRScript : MonoBehaviour {
 	public void QuitGame () {
 		Debug.Log ("quit game...");
 	}
-<<<<<<< HEAD
-
-	public void TPP () {
-		Time.timeScale = 1f;
-		AudioListener.volume = 1F;
-		PauseMenuUI.SetActive (true);
-
-		TPPMenuUI.SetActive (false);
-		FPPMenuUI.SetActive (false);
-
-		TPPCamera.SetActive (true);
-		FPPCamera.SetActive (false);
-
-		PlayerAvatar.SetActive (true);
-
-		PlayerMenuUI.GetComponent<FPPMovement> ().enabled = false;
-		PlayerMenuUI.GetComponent<PlayerMovement> ().enabled = true;
-
-		IsPaused = false;
-	}
-
-	public void FPP () {
-		Time.timeScale = 1f;
-		AudioListener.volume = 1F;
-		PauseMenuUI.SetActive (true);
-
-		TPPMenuUI.SetActive (false);
-		FPPMenuUI.SetActive (false);
-
-		TPPCamera.SetActive (false);
-		FPPCamera.SetActive (true);
-
-		PlayerAvatar.SetActive (false);
-
-		PlayerMenuUI.GetComponent<FPPMovement> ().enabled = true;
-		PlayerMenuUI.GetComponent<PlayerMovement> ().enabled = false;
-
-		IsPaused = false;
-	}
-=======
->>>>>>> 207ad6f5145d6df9c59e8f4ab687efc6e272c0be
 }
