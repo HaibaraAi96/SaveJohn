540d7d068cbca2e854f41143d92d4ab3e343f0dc
question0914 <895820518@qq.com>
2018-04-18 13:29:08 -0700

    Revert "Merge branch '526' of https://github.com/lovolLiu/SaveJohn into 526"
    This reverts commit e48674b265c0ff9063bf17f731b6515471d1f67b, reversing
    changes made to 47609f976adaf847d90c53156d4c56601dc8831f.

--- Assets/PRScript.cs
+++ Assets/PRScript.cs
@@ -9,7 +9,16 @@ public class PRScript : MonoBehaviour {
 	public static bool IsPaused = false;
 	public GameObject PauseMenuUI;
 	public GameObject ResumeMenuUI;
+<<<<<<< HEAD
 	public GameObject SGMenuUI;
+=======
+	public GameObject FPPMenuUI;
+	public GameObject TPPMenuUI;
+	public GameObject FPPCamera;
+	public GameObject TPPCamera;
+	public GameObject PlayerAvatar;
+	public GameObject PlayerMenuUI;
+>>>>>>> parent of e48674b2... Merge branch '526' of https://github.com/lovolLiu/SaveJohn into 526
 
 	// Use this for initialization
 	void Start () {
@@ -46,13 +55,21 @@ public class PRScript : MonoBehaviour {
 		IsPaused = true;
 	}
 
+<<<<<<< HEAD
 	public void StartGame () {
+=======
+	/*public void StartGame () {
+>>>>>>> parent of e48674b2... Merge branch '526' of https://github.com/lovolLiu/SaveJohn into 526
 		Time.timeScale = 1f;
 		AudioListener.volume = 1F;
 		PauseMenuUI.SetActive (true);
 		SGMenuUI.SetActive (false);
 		IsPaused = false;
+<<<<<<< HEAD
 	}
+=======
+	}*/
+>>>>>>> parent of e48674b2... Merge branch '526' of https://github.com/lovolLiu/SaveJohn into 526
 
 	public void LoadMenu () {
 		Debug.Log ("loading menu...");
@@ -61,4 +78,45 @@ public class PRScript : MonoBehaviour {
 	public void QuitGame () {
 		Debug.Log ("quit game...");
 	}
+<<<<<<< HEAD
+=======
+
+	public void TPP () {
+		Time.timeScale = 1f;
+		AudioListener.volume = 1F;
+		PauseMenuUI.SetActive (true);
+
+		TPPMenuUI.SetActive (false);
+		FPPMenuUI.SetActive (false);
+
+		TPPCamera.SetActive (true);
+		FPPCamera.SetActive (false);
+
+		PlayerAvatar.SetActive (true);
+
+		PlayerMenuUI.GetComponent<FPPMovement> ().enabled = false;
+		PlayerMenuUI.GetComponent<PlayerMovement> ().enabled = true;
+
+		IsPaused = false;
+	}
+
+	public void FPP () {
+		Time.timeScale = 1f;
+		AudioListener.volume = 1F;
+		PauseMenuUI.SetActive (true);
+
+		TPPMenuUI.SetActive (false);
+		FPPMenuUI.SetActive (false);
+
+		TPPCamera.SetActive (false);
+		FPPCamera.SetActive (true);
+
+		PlayerAvatar.SetActive (false);
+
+		PlayerMenuUI.GetComponent<FPPMovement> ().enabled = true;
+		PlayerMenuUI.GetComponent<PlayerMovement> ().enabled = false;
+
+		IsPaused = false;
+	}
+>>>>>>> parent of e48674b2... Merge branch '526' of https://github.com/lovolLiu/SaveJohn into 526
 }
